937199c6c817aac87197341100e580d495556a68
Luis <luis.martinez-molina@capgemini.com>
2022-01-20 16:29:31 +0100

    Rama Master

---
+++ src/main/java/com/capgemini/GestorVetApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorVetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorVetApplication.class, args);
+	}
+
+}
