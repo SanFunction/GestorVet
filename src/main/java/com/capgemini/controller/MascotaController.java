fcd04491bf973d145de807074c689e6f8683d737
Javier <javier.lopez-mora-gimenez@capgemini.com>
2022-01-26 10:18:05 +0100

    añadidos los crossOrigin para angular

--- src/main/java/com/capgemini/controller/MascotaController.java
+++ src/main/java/com/capgemini/controller/MascotaController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import com.capgemini.services.IMascotaService;
 
 @RestController
 @RequestMapping(value = "/mascota")
+@CrossOrigin(origins = "http://localhost:4200")
 public class MascotaController {
 
 	@Autowired
